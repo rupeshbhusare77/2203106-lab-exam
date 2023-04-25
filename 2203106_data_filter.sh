f229d591f226f2d5c0aec1c06aca555817ae5c59
rupeshbhusare77 <rupeshbhusare1232@gmail.com>
2023-04-25 11:49:59 +0530

    again commited

--- 2203106_data_filter.sh
+++ 2203106_data_filter.sh
@@ -1,3 +1,5 @@
-file=$1  #takes 1st aruguments
-field_name=$2
-field_value=$3
+echo "Data filter script"
+file=$1  #takes 1st positional aruguments
+field_name=$2 #takes 2nd psitional arguments
+field_value=$3 #takes 3rd positional arguments
+
